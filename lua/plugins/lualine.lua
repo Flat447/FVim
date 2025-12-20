9c5276c7d59fdb5177c2910888c79d8de2a1e687
FLAT447 <alexey_mezentsev@list.ru>
2025-12-20 16:42:46 +0500

    Update lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,6 +1,9 @@
 require('lualine').setup({
     options = {
         component_separators = { left = '', right = ''},
-        section_separators = { left = '', right = ''}
+        section_separators = { left = '', right = ''},
+        disabled_filetypes = {
+            statusline = {'alpha'},
+        }
     }
 })
